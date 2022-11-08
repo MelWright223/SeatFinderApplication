4e35baa6b9d944be24ac5430a0d6e02ae9c826f0
Mel Wright <melissa.wright@students.plymouth.ac.uk>
2022-11-08 16:47:31 +0000

    Added function to enable the AI to get train model based on maximum carriage space at intermediate stations

--- TrainCarriageData.py
+++ TrainCarriageData.py
@@ -4,7 +4,6 @@ connString = DatabaseConn.mydb
 getData = connString.cursor(buffered=True)
 
 
-
 def has_value(cursor, table, column, value):
     query = ('Select * From {} Where {} = {}'.format(table, column, value))
     cursor.execute(query)
@@ -17,8 +16,8 @@ def cell_value(cursor, column, table, specificColumn, value):
     return cursor
 
 
-def getDescID():
-    value = cell_value(getData, "DescriptionID", "routes", "NumOfInterStations", 5)
+def getDescID(numOfInterStations):
+    value = cell_value(getData, "DescriptionID", "routes", "NumOfInterStations", numOfInterStations)
     descriptionId = [*value]
     for i in descriptionId:
         routeId = [*i]
@@ -26,9 +25,8 @@ def getDescID():
     return descId
 
 
-def routeInfo():
-    getDescID()
-    valueOfCell = has_value(getData, "route_description_5_stations", "DescriptionID", getDescID()[0])
+def routeInfoFiveStations():
+    valueOfCell = has_value(getData, "route_description_5_stations", "DescriptionID", getDescID(5)[0])
     dataOfCell = [*valueOfCell]
     routeData = []
     for x in dataOfCell:
@@ -37,12 +35,22 @@ def routeInfo():
     return routeData
 
 
-def stationInfo():
+def routeInfoEightStations():
+    valueOfCell = has_value(getData, "route_description_9_stations", "DescriptionID", getDescID(8)[0])
+    dataOfCell = [*valueOfCell]
+    routeData = []
+    for x in dataOfCell:
+        routeData.append(x)
+        # desID, depStatID, first, sec, third, four, five, six, seven, eight, destin = routeData
+    return routeData
+
+
+def fiveStationInfo(): # if the route has 5 intermediate stations between the departure station and the destination
     t = 0
     carriages = []
 
     station_data = []
-    for val in routeInfo():
+    for val in routeInfoFiveStations():
         if has_value(getData, "train_stations", "StationId", val[1]):
             x = [*val]
             x.pop(0)
@@ -56,7 +64,43 @@ def stationInfo():
     return carriages
 
 
-# use station id in station table
-# get maximum carriages for each dedicated station
-# store all maximum carriages in a list
+def eightStationInfo(): # if the route has 8 intermediate stations between departure station and destination station
+    t = 0
+    carriages = []
 
+    station_data = []
+    for val in routeInfoEightStations():
+        if has_value(getData, "train_stations", "StationId", val[1]):
+            x = [*val]
+            x.pop(0)
+            station_data.append(x)
+    for val in station_data:
+        for val_two in val:
+            for i in has_value(getData, "train_stations", "StationId", val_two):
+                stations = [*i]
+                stationID, stationName, maxCarriages = stations
+                carriages.append(maxCarriages)
+    return carriages
+
+
+def getTrainModel():
+    carriageData =[]
+    if getDescID(8):
+        carriages = eightStationInfo()
+        for j in carriages:
+            carriagesTrain = j
+            trainModel = has_value(getData, "train_models", "MaximumCarriages", carriagesTrain)
+            carriageData.append([*trainModel])
+            if carriageData.count(j) > 1:
+                print(carriageData)
+
+    if getDescID(5):
+        carriages = fiveStationInfo()
+        for j in carriages:
+            carriagesTrain = j
+            trainModel = has_value(getData, "train_models", "MaximumCarriages", carriagesTrain)
+            carriageData = [*trainModel]
+    print(carriageData)
+
+
+getTrainModel()
